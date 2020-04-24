eaf3e827847c5d44337b8f99ddfc76857845f666
✨小透明・宸✨ <1272742391@qq.com>
2020-04-24 10:48:44 +0800

    资词为Phar打包使用bz2压缩

--- build-phar.php
+++ build-phar.php
@@ -1,5 +1,8 @@
 <?php
 @unlink('cloud-clipboard.phar');
+@unlink('cloud-clipboard-gz.phar');
+@unlink('cloud-clipboard-bz2.phar');
+
 $phar = new Phar('cloud-clipboard.phar');
 
 foreach ([
@@ -46,10 +49,39 @@ foreach ([
         if ($excluded) continue;
 
         $localname = preg_replace('/^(server\/)/', '', $file);
-        echo $file . "\n";
+
+        $minify = false;
+        if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+            $minify = true;
+            $minified = php_strip_whitespace($file);
+            $file .= '.min';
+            file_put_contents($file, $minified);
+        } else if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+            $minify = true;
+            $minified = json_encode(json_decode(file_get_contents($file), true), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            $file .= '.min';
+            file_put_contents($file, $minified);
+        }
+
         $phar->addFile($file, $localname);
+        if ($minify) unlink($file);
+        echo $file . "\n";
     }
 }
 
 $phar->setStub($phar->createDefaultStub('main.php'));
-$phar->compressFiles(Phar::GZ);
+// $phar->extractTo('extract');
+
+if (extension_loaded('zlib')) {
+    copy('cloud-clipboard.phar', 'cloud-clipboard-gz.phar');
+    (new Phar('cloud-clipboard-gz.phar'))->compressFiles(Phar::GZ);
+} else {
+    echo "GZ compression is not enabled.\n";
+}
+
+if (extension_loaded('bz2')) {
+    copy('cloud-clipboard.phar', 'cloud-clipboard-bz2.phar');
+    (new Phar('cloud-clipboard-bz2.phar'))->compressFiles(Phar::BZ2);
+} else {
+    echo "BZ2 compression is not enabled.\n";
+}
