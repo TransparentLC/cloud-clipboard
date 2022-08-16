8397bdf33ed5fcfcba381c665801a4776a4792c1
✨小透明・宸✨ <1272742391@qq.com>
2022-08-16 21:26:55 +0800

    添加brotli预压缩

--- client/after-build.js
+++ client/after-build.js
@@ -10,14 +10,24 @@ const files = await new Promise((resolve, reject) => glob(
     (error, files) => error ? reject(error) : resolve(files)
 ));
 
-await Promise.all(files.map(e => new Promise((resolve, reject) => {
-    const readStream = fs.createReadStream(e);
-    const writeStream = fs.createWriteStream(e + '.gz');
-    readStream
-        .pipe(zlib.createGzip({ level: 9 }))
-        .pipe(writeStream)
-        .on('finish', error => error ? reject(error) : resolve());
-})));
+await Promise.all(files.map(e => [
+    new Promise((resolve, reject) => {
+        const readStream = fs.createReadStream(e);
+        const writeStream = fs.createWriteStream(e + '.gz');
+        readStream
+            .pipe(zlib.createGzip({ level: 9 }))
+            .pipe(writeStream)
+            .on('finish', error => error ? reject(error) : resolve());
+    }),
+    new Promise((resolve, reject) => {
+        const readStream = fs.createReadStream(e);
+        const writeStream = fs.createWriteStream(e + '.br');
+        readStream
+            .pipe(zlib.createBrotliCompress({ params: { [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY }}))
+            .pipe(writeStream)
+            .on('finish', error => error ? reject(error) : resolve());
+    }),
+]).flat());
 
 fs.rmSync('../server/static', { recursive: true });
 fs.rmSync('../server-node/static', { recursive: true });
