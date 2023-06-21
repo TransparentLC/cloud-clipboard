578f5b0cb6a0c06fdf14bb5a15c76d53f5b4e949
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2023-06-21 09:33:44 +0800

    没有配置时写入默认配置
    Close #13

--- server-node/app/config.js
+++ server-node/app/config.js
@@ -2,6 +2,31 @@ import crypto from 'node:crypto';
 import fs from 'node:fs';
 import path from 'node:path';
 
+const defaultConfigPath = path.join(process.cwd(), 'config.json');
+
+if (!process.argv[2] && !fs.existsSync(defaultConfigPath)) {
+    console.log(`\x1b[93mConfig file "${defaultConfigPath}" does not exist.\x1b[39m`);
+    console.log('\x1b[93mA default config file is created and used. Check the descriptions in the repository\'s README.md to modify it.\x1b[39m');
+    fs.writeFileSync(defaultConfigPath, JSON.stringify({
+        server: {
+            host: [],
+            port: 9501,
+            key: null,
+            cert: null,
+            history: 10,
+            auth: false,
+        },
+        text: {
+            limit: 4096,
+        },
+        file: {
+            expire: 3600,
+            chunk: 2097152,
+            limit: 268435456,
+        },
+    }, null, 4));
+}
+
 /**
  * @type {{
  *  server: {
@@ -22,12 +47,13 @@ import path from 'node:path';
  *  },
  * }}
  */
-const config = JSON.parse(fs.readFileSync(process.argv[2] || path.join(process.cwd(), 'config.json')));
+const config = JSON.parse(fs.readFileSync(process.argv[2] || defaultConfigPath));
 
 if (config.server.auth === true) {
     config.server.auth = '';
-    for (let i = 0; i < 6; i++)
-    config.server.auth += '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'[crypto.randomInt(62)];
+    for (let i = 0; i < 6; i++) {
+        config.server.auth += '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'[crypto.randomInt(62)];
+    }
 }
 if (config.server.auth) {
     config.server.auth = config.server.auth.toString();
