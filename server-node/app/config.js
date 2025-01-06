de011076f428ab3aa5ac2a27d862ca6c64c24d86
jonny <coorhook@gmail.com>
2025-01-06 19:42:16 +0800

    Update config.js

--- server-node/app/config.js
+++ server-node/app/config.js
@@ -17,8 +17,8 @@ if (!process.argv[2] && !fs.existsSync(defaultConfigPath)) {
             cert: null,
             history: 10,
             auth: false,
-            historyPath: null,
-            storagePath: null,
+            historyDir: null,
+            storageDir: null,
         },
         text: {
             limit: 4096,
