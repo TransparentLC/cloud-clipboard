76f6c948155a18ce07943b6e162a08e253924983
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2025-03-20 13:00:05 +0800

    要恰饭的嘛

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,12 +2,10 @@
     <v-container fill-height>
         <v-row justify="center" align="center" class="pa-3">
             <div class="text-center">
-                <div class="headline my-4">Cloud Clipboard {{$root.config.version}}</div>
-                在局域网内互传纯文本和文件的云剪贴板
-                <br>
-                即开即用的网页版，无须安装 APP
-                <br>
-                <a href="https://github.com/TransparentLC/cloud-clipboard" target="_blank">GitHub</a>
+                <p class="headline my-4">Cloud Clipboard {{$root.config.version}}</p>
+                <p>在局域网内互传纯文本和文件的云剪贴板<br>即开即用的网页版，无须安装 APP</p>
+                <p><a href="https://github.com/TransparentLC/cloud-clipboard" target="_blank"><img src="https://img.shields.io/github/stars/TransparentLC/cloud-clipboard.svg?style=social"></a></p>
+                <p class="text--secondary"><small>如果你觉得这个工具很好用，欢迎<a href="https://i.akarin.dev/donate/" target="_blank">捐赠支持开发者</a>～</small></p>
             </div>
         </v-row>
     </v-container>
